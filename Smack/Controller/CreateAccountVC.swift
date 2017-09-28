7d2932baa24658e9733eaf0bdc819927a8ab01fd
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2017-09-28 13:18:12 -0700

    Added create account view

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,24 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Daniel Stahl on 9/28/17.
+//  Copyright © 2017 Daniel Stahl. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+}
