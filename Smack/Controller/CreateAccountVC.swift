da37aacd6471f85d72f412637ddd62fa520e11c3
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2017-10-02 13:43:58 -0700

    connecting api with app

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,7 +16,6 @@ class CreateAccountVC: UIViewController {
         
     }
 
-    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
