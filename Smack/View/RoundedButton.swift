e9d231a73155ad61051d689ea5279a0321de694f
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2017-10-06 12:27:09 -0700

    Registered user

---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Daniel Stahl on 10/6/17.
+//  Copyright © 2017 Daniel Stahl. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+   
+    override func awakeFromNib() {
+        self.setupView()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
