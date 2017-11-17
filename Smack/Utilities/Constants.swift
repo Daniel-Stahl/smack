21679c05ab0ab6f367f2bbd961a515e7d0cb39c7
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2017-11-16 20:05:07 -0800

    login function is working

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://smackapp-ds.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
 //Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.2783474392, green: 0.4461805556, blue: 0.9147135417, alpha: 0.5)
@@ -34,5 +35,10 @@ let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
 let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
     "Content-Type": "application/json; charset=utf-8"
 ]
